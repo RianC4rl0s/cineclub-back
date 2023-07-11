80914edb29393aad258b6c2300bf27b8a6aa13fe
RianC4rl0s <riancarlos.sl@gmail.com>
2023-07-11 10:17:02 -0300

    feat:create project

---
+++ src/main/java/com/cineclub/cineclubback/CineclubBackApplication.java
@@ -0,0 +1,13 @@
+package com.cineclub.cineclubback;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CineclubBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CineclubBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cineclub/cineclubback/CineclubBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cineclub.cineclubback;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CineclubBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
